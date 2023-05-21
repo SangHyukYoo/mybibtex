70749b90e1d7c855820190e5d3b343c5bffe81ff
SangHyuk Yoo <sanghyuk.yoo.08@gmail.com>
2023-05-21 21:42:05 +0900

    convert bib to html by python

---
+++ bib2html.py
@@ -0,0 +1,17 @@
+import bibtexparser
+from jinja2 import Environment, FileSystemLoader
+
+# BibTeX 파일을 파싱합니다.
+with open('ref.bib') as bibtex_file:
+    bib_database = bibtexparser.load(bibtex_file)
+
+# Jinja2 템플릿을 로드합니다.
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template('template.html')
+
+# 파싱된 데이터를 템플릿에 적용합니다.
+html_output = template.render(entries=bib_database.entries)
+
+# 결과를 HTML 파일로 저장합니다.
+with open('ref.html', 'w') as html_file:
+    html_file.write(html_output)
